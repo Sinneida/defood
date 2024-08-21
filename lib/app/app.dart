c73ac3b0cf5b4d90259a2b653cca8d4ee5fa8843
sinneida <55593562+Sinneida@users.noreply.github.com>
2024-08-21 03:07:44 +0200

    add getit deps

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,9 @@
+import 'package:defood/services/custom_themes_service.dart';
+import 'package:defood/services/font_importer_service.dart';
+import 'package:defood/services/logs_service.dart';
+import 'package:defood/services/paths_service.dart';
+import 'package:defood/services/settings_service.dart';
+import 'package:defood/services/updater_service.dart';
 import 'package:defood/ui/bottom_sheets/notice/notice_sheet.dart';
 import 'package:defood/ui/dialogs/info_alert/info_alert_dialog.dart';
 import 'package:defood/ui/views/home/home_view.dart';
@@ -14,6 +20,12 @@ import 'package:stacked_services/stacked_services.dart';
     LazySingleton(classType: BottomSheetService),
     LazySingleton(classType: DialogService),
     LazySingleton(classType: NavigationService),
+    LazySingleton(classType: CustomThemesService),
+    InitializableSingleton(classType: SettingsService),
+    LazySingleton(classType: PathsService),
+    LazySingleton(classType: LogsService),
+    LazySingleton(classType: UpdaterService),
+    LazySingleton(classType: FontImporterService),
 // @stacked-service
   ],
   bottomsheets: [
