817d8ebe6f2507239b75109e6e4f410c80c07c72
sinneida <55593562+Sinneida@users.noreply.github.com>
2024-08-27 01:45:26 +0200

    simplify auth since it's moved to service

--- lib/ui/views/login/login_viewmodel.dart
+++ lib/ui/views/login/login_viewmodel.dart
@@ -1,56 +1,68 @@
-import 'package:defood/utils/envs.dart';
+import 'package:defood/app/app.locator.dart';
+import 'package:defood/app/app.router.dart';
+import 'package:defood/app/app.snackbar.dart';
+import 'package:defood/models/errors/auth_error.dart';
+import 'package:defood/services/auth_service.dart';
 import 'package:defood/utils/function_name.dart';
 import 'package:flutter_logs/flutter_logs.dart';
 import 'package:stacked/stacked.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:stacked_services/stacked_services.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class LoginViewModel extends BaseViewModel {
-  Future<void> signInWithGoogle() async {
+class LoginViewModel extends StreamViewModel<AuthState> {
+  final _auth = locator<AuthService>();
+  final _snackbar = locator<SnackbarService>();
+  final _nav = locator<NavigationService>();
+
+  Future<void> signIn() async {
     try {
-      final webClientId = Env.webClientId;
-      final clientId = Env.clientId;
-      final data = GoogleSignIn(
-        signInOption: SignInOption.standard,
-        clientId: clientId,
-        serverClientId: webClientId,
+      await _auth.signIn();
+      _snackbar.showCustomSnackBar(
+        message:
+            'Logged in as ${_auth.account?.session?.user.email ?? '<error>'}',
+        variant: SnackbarType.info,
       );
-      final googleUser = await data.signIn();
-      final googleAuth = await googleUser!.authentication;
-      final accessToken = googleAuth.accessToken;
-      final idToken = googleAuth.idToken;
-
-      if (accessToken == null) {
-        throw 'No Access Token found.';
-      }
-      if (idToken == null) {
-        throw 'No ID Token found.';
-      }
-
-      final account = await Supabase.instance.client.auth.signInWithIdToken(
-        provider: OAuthProvider.google,
-        idToken: idToken,
-        accessToken: accessToken,
-      );
-      print(account);
+      _nav.clearStackAndShow(Routes.navigationView);
     } catch (e) {
       FlutterLogs.logError(
         runtimeType.toString(),
         getFunctionName(),
         'Failed to login with Google: $e',
       );
+      _snackbar.showCustomSnackBar(
+        message: e is AuthError ? e.message : e.toString(),
+        variant: SnackbarType.info,
+      );
     }
   }
 
   Future<void> signOut() async {
     try {
-      await Supabase.instance.client.auth.signOut();
+      await _auth.signOut();
+      _snackbar.showCustomSnackBar(
+        message: 'Logged out from Supabase',
+        variant: SnackbarType.info,
+      );
     } catch (e) {
       FlutterLogs.logError(
         runtimeType.toString(),
         getFunctionName(),
         'Failed to log out: $e',
       );
+      _snackbar.showCustomSnackBar(
+        message: e is AuthError ? e.message : e.toString(),
+        variant: SnackbarType.info,
+      );
+    }
+  }
+
+  @override
+  Stream<AuthState> get stream => _auth.authClient.onAuthStateChange;
+
+  @override
+  void onData(AuthState? data) {
+    if (data != null) {
+      data.event == AuthChangeEvent.signedIn;
     }
   }
 }
