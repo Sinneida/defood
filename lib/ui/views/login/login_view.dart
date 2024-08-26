b1e15db8e119ae1a694da086111fa2897f5a4ecb
sinneida <55593562+Sinneida@users.noreply.github.com>
2024-08-27 00:34:36 +0200

    basic login to supabase (WIP)

--- lib/ui/views/login/login_view.dart
+++ lib/ui/views/login/login_view.dart
@@ -1,10 +1,12 @@
+import 'package:defood/gen/strings.g.dart';
+import 'package:defood/ui/common/ui_helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 import 'login_viewmodel.dart';
 
 class LoginView extends StackedView<LoginViewModel> {
-  const LoginView({Key? key}) : super(key: key);
+  const LoginView({super.key});
 
   @override
   Widget builder(
@@ -13,9 +15,68 @@ class LoginView extends StackedView<LoginViewModel> {
     Widget? child,
   ) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+            pinned: true,
+            expandedHeight: 90,
+            automaticallyImplyLeading: false,
+            flexibleSpace: FlexibleSpaceBar(
+              titlePadding: const EdgeInsets.only(bottom: 16, left: 20),
+              title: Text(
+                'Login',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimaryContainer,
+                ),
+              ),
+            ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Card.outlined(
+                    child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          children: [
+                            ListTile(
+                              title: Text(
+                                t.login.description,
+                              ),
+                            ),
+                            verticalSpaceTiny,
+                            Card(
+                              clipBehavior: Clip.antiAlias,
+                              elevation: 1,
+                              child: ListTile(
+                                onTap: () => viewModel.signInWithGoogle(),
+                                leading: const Icon(Icons.login),
+                                title: Text(t.login.login_button),
+                              ),
+                            ),
+                            verticalSpaceTiny,
+                            Card(
+                              clipBehavior: Clip.antiAlias,
+                              elevation: 1,
+                              child: ListTile(
+                                onTap: () => viewModel.signOut(),
+                                leading: const Icon(Icons.logout),
+                                title: const Text('Log out'),
+                              ),
+                            ),
+                          ],
+                        )),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/ui/views/login/login_viewmodel.dart
+++ lib/ui/views/login/login_viewmodel.dart
@@ -1,3 +1,56 @@
+import 'package:defood/utils/envs.dart';
+import 'package:defood/utils/function_name.dart';
+import 'package:flutter_logs/flutter_logs.dart';
 import 'package:stacked/stacked.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class LoginViewModel extends BaseViewModel {}
+class LoginViewModel extends BaseViewModel {
+  Future<void> signInWithGoogle() async {
+    try {
+      final webClientId = Env.webClientId;
+      final clientId = Env.clientId;
+      final data = GoogleSignIn(
+        signInOption: SignInOption.standard,
+        clientId: clientId,
+        serverClientId: webClientId,
+      );
+      final googleUser = await data.signIn();
+      final googleAuth = await googleUser!.authentication;
+      final accessToken = googleAuth.accessToken;
+      final idToken = googleAuth.idToken;
+
+      if (accessToken == null) {
+        throw 'No Access Token found.';
+      }
+      if (idToken == null) {
+        throw 'No ID Token found.';
+      }
+
+      final account = await Supabase.instance.client.auth.signInWithIdToken(
+        provider: OAuthProvider.google,
+        idToken: idToken,
+        accessToken: accessToken,
+      );
+      print(account);
+    } catch (e) {
+      FlutterLogs.logError(
+        runtimeType.toString(),
+        getFunctionName(),
+        'Failed to login with Google: $e',
+      );
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await Supabase.instance.client.auth.signOut();
+    } catch (e) {
+      FlutterLogs.logError(
+        runtimeType.toString(),
+        getFunctionName(),
+        'Failed to log out: $e',
+      );
+    }
+  }
+}
