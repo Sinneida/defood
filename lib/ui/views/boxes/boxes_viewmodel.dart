ba9b39a311edf0fe6c651f5e74f9ca9c33cd331a
sinneida <55593562+Sinneida@users.noreply.github.com>
2024-08-30 03:45:34 +0200

    log in when already signed in

--- lib/ui/views/boxes/boxes_viewmodel.dart
+++ lib/ui/views/boxes/boxes_viewmodel.dart
@@ -1,9 +1,39 @@
 import 'package:defood/app/app.locator.dart';
+import 'package:defood/app/app.snackbar.dart';
+import 'package:defood/models/errors/auth_error.dart';
 import 'package:defood/services/auth_service.dart';
+import 'package:defood/services/settings/auth_settings_service.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class BoxesViewModel extends BaseViewModel {
   final _auth = locator<AuthService>();
+  final _authSettings = locator<AuthSettingsService>();
+  final _snackbar = locator<SnackbarService>();
+
+  bool get hasSignedIn => _authSettings.hasSignedIn;
+
+  bool _logginIn = false;
+  bool get logginIn => _logginIn;
+
+  Future<void> signIn() async {
+    try {
+      _logginIn = true;
+      rebuildUi();
+      await _snackbar.showCustomSnackBar(
+        message: 'Logging in...',
+        variant: SnackbarType.progress,
+      );
+      await _auth.signIn();
+      _logginIn = false;
+      rebuildUi();
+    } catch (e) {
+      _snackbar.showCustomSnackBar(
+        message: e is AuthError ? e.message : e.toString(),
+        variant: SnackbarType.info,
+      );
+    }
+  }
 
   String? get avatar {
     return _auth.account?.user?.userMetadata?['avatar_url'];
