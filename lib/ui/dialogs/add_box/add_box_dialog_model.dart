3c1d14e126ba5dac716ef8637b85ebe35f89c247
sinneida <55593562+Sinneida@users.noreply.github.com>
2024-09-01 23:33:11 +0200

    update info in add box model

--- lib/ui/dialogs/add_box/add_box_dialog_model.dart
+++ lib/ui/dialogs/add_box/add_box_dialog_model.dart
@@ -1,7 +1,8 @@
 import 'package:stacked/stacked.dart';
 
 class AddBoxDialogModel extends FormViewModel {
-  final message = 'Box is an abstraction of a place, where you keep your food products.';
+  final message =
+      'Please provide a name for a new box. Box is a place where you keep your food products, i.e. fridge, closet or drawer.';
   // final urlMessage = '';
   // final appMessage = 'messenger';
 }
