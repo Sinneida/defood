0c9e1aaba51c07b5941e2803e145f4b780ef1da1
sinneida <55593562+Sinneida@users.noreply.github.com>
2024-09-02 21:59:41 +0200

    change mixin to abstract base class

--- lib/services/settings/base/helpers.dart
+++ lib/services/settings/base/helpers.dart
@@ -1,6 +1,8 @@
-import 'package:defood/utils/function_name.dart';
+import 'dart:io';
+
+import 'package:defood/services/settings/appearance_settings_service.dart';
+import 'package:defood/utils/logger_helper.dart';
 import 'package:device_info_plus/device_info_plus.dart';
-import 'package:flutter_logs/flutter_logs.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -9,16 +11,12 @@ typedef AppPackageInfo = ({String appName, String buildNumber, String version});
 /// Mixin for `SettingsService` class to provide
 /// helpful methods not related explicitly to settings items
 /// themselves
-mixin SettingsHelpers {
+abstract base class SettingsHelpers with LoggerHelper {
   Future<bool> installGranted() async {
     try {
       return await Permission.requestInstallPackages.status.isGranted;
     } catch (e) {
-      FlutterLogs.logError(
-        runtimeType.toString(),
-        getFunctionName(),
-        'Cannot write to this folder',
-      );
+      logError('Cannot write to this folder', e);
     }
     return false;
   }
@@ -35,5 +33,13 @@ mixin SettingsHelpers {
   Future<bool> checkForAndroid12Plus() async {
     final deviceInfo = await DeviceInfoPlugin().androidInfo;
     return deviceInfo.version.sdkInt >= 31;
+  }
+
+  Future<void> ensureAppDirExists() async {
+    final defaultDir = Directory(defaultDirPath);
+    final isDefaultDir = await defaultDir.exists();
+    if (!isDefaultDir) {
+      await defaultDir.create();
+    }
   }
 }
