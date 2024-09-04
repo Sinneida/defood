b35f5e1b4d28238f9ecdc3d3cbe7b457bee3de8a
sinneida <55593562+Sinneida@users.noreply.github.com>
2024-09-04 03:44:05 +0200

    showcase of products

--- lib/ui/views/products/widgets/product_card.dart
+++ lib/ui/views/products/widgets/product_card.dart
@@ -1,4 +1,5 @@
 import 'package:defood/models/product.dart';
+import 'package:defood/ui/common/ui_helpers.dart';
 import 'package:defood/ui/views/products/widgets/product_card_model.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -35,13 +36,61 @@ class ProductCard extends StackedView<ProductCardModel> {
       child: Card(
         clipBehavior: Clip.antiAlias,
         surfaceTintColor: Theme.of(context).colorScheme.primary,
-        child: ListTile(
+        child: ExpansionTile(
+          initiallyExpanded: true,
+          expandedAlignment: Alignment.centerLeft,
+          expandedCrossAxisAlignment: CrossAxisAlignment.start,
+          childrenPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(16))),
+          collapsedShape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(16))),
           title: Text(product.name),
-          subtitle: Text(
-            DateFormat('dd-mm-yyyy').format(
-              product.expirationDate,
+          // subtitle: Text(
+          //   'Expiration date: ${DateFormat('dd-MM-yyyy').format(
+          //     product.expirationDate,
+          //   )}\nExpiration type: ${product.expirationType}',
+          // ),
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Expiration date:\n${DateFormat('dd-MM-yyyy').format(
+                      product.expirationDate,
+                    )}'),
+                    verticalSpaceTiny,
+                    Text('Expiration type:\n${product.expirationType}'),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Price:\n${product.price} PLN'),
+                    verticalSpaceTiny,
+                    Text('Amount:\n${product.amount}')
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('Created:\n${DateFormat('dd-MM-yyyy HH:mm').format(
+                      product.createdAt,
+                    )}'),
+                    verticalSpaceTiny,
+                    Text('Updated:\n${DateFormat('dd-MM-yyyy HH:mm').format(
+                      product.updatedAt,
+                    )}'),
+                  ],
+                ),
+              ],
             ),
-          ),
+          ],
         ),
       ),
     );
