e3c182bcec99828f3769db729406be7c45549af4
sinneida <55593562+Sinneida@users.noreply.github.com>
2024-09-12 03:34:49 +0200

    get data from camera view & pass it to off service

--- lib/ui/views/products/products_viewmodel.dart
+++ lib/ui/views/products/products_viewmodel.dart
@@ -3,14 +3,17 @@ import 'dart:async';
 import 'package:defood/app/app.locator.dart';
 import 'package:defood/models/product.dart';
 import 'package:defood/services/database_service.dart';
+import 'package:defood/services/off_service.dart';
 import 'package:defood/ui/views/camera/camera_view.dart';
 import 'package:defood/utils/notification_helper.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class ProductsViewModel extends BaseViewModel with NotificationHelper {
   final _db = locator<DatabaseService>();
   final _nav = locator<NavigationService>();
+  final _off = locator<OpenFoodFactsService>();
 
   final List<Product> _products = [];
   List<Product> get products => _products;
@@ -27,7 +30,9 @@ class ProductsViewModel extends BaseViewModel with NotificationHelper {
 
   Future<void> showCamera() async {
     try {
-      _nav.navigateToView(const CameraView());
+      final barcode = await _nav.navigateToView<Barcode>(const CameraView());
+      print('Product barcode: ${barcode.toString()}');
+      await _off.getProductData(barcode!.rawValue!);
     } catch (e) {
       notifyError('Failed to show camera view');
       notifyError(e.toString());
